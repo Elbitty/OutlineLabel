af338f2ffaa125d57f7234078add0996cab9673b
Elbitty <conor123481@gmail.com>
2017-07-03 05:19:45 +0900

    Update FrmMain.vb

--- Karaoke0/FrmMain.vb
+++ Karaoke0/FrmMain.vb
@@ -1,7 +1,6 @@
 ﻿Public Class FrmMain
     Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.BackColor = Color.FromArgb(255, 7, 11, 5)
-        Me.TransparencyKey = Color.FromArgb(255, 7, 11, 5)
+
     End Sub
 
 End Class
